237d24aa7d224f3781d8ec972aa5761e4336fe5e
KazuakiTokunaga <ktokunaga1993@gmail.com>
2022-06-15 17:04:03 +0900

    change output

---
+++ library/programming_contest_challenge/062_個数制限付き部分和.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using pii = pair<int, int>;
+#define rep(i,a,b) for(int i=a;i<b;i++)
+#define rrep(i,a,b) for(int i=a;i>=b;i--)
+#define all(x) (x).begin(),(x).end()
+
+int main() {
+    int n;
+    cin >> n;
+    
+    int res = 0;
+    cout << res << endl;
+}
